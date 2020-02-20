bcd5516989ad238a5ec7c1423d4e267dcc86ec18
Serpantes <4uteppp@gmail.com>
2020-02-20 16:33:23 +0300

    lesson3 v1

--- main.go
+++ main.go
@@ -1,109 +1,189 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"math/big"
+	"io/ioutil"
 )
 
-var prime [99]int
-var ns [540][2]int
-
-func fillNumb() { //a
-	for i := 0; i < len(ns); i++ {
-		ns[i][0] = i + 2
-	}
-	for i := 0; i < len(ns); i++ {
-		ns[i][1] = 0
-	}
+type vehilce struct {
+	model                     string
+	volume, volumeOccup, year int
+	isRunning, isWindowOpen   bool
+}
+type car struct {
+	model        string
+	volume       int
+	volumeOccup  int
+	year         int
+	isRunning    bool
+	isWindowOpen bool
 }
 
-func mark(n int) {
-	p := 2                                //b
-	for marks := 0; marks <= n; marks++ { //e
-		for p2 := p * p; p2 <= len(ns); p2 = p2 + p {
-			ns[p2-2][1] = 1 //c
-		}
-		for i := 0; i <= 539; i++ {
-			if ns[i][1] == 0 && ns[i][0] > p {
-				p = ns[i][0] //d
-				break
-			}
-		}
-	}
+type queue struct {
+	nodes []*node
+	size  int
+	front int
+	back  int
+	count int
 }
-func fillPrime() { //4*
-	mark(100)
-	i := 0
-	count := 0
-	for count < len(prime) {
-		if ns[i][1] == 0 {
-			prime[count] = ns[i][0]
-			count++
-		}
-		i++
-	}
-}
-func chet(a int) bool {
-	if a%2 == 0 {
-		return true
-	}
-	return false
-}
-func chetT(a int) bool {
-	if a%3 == 0 {
-		return true
-	}
-	return false
-}
-func fibPrint(n int) {
-	for i := 0; i < n; i++ {
-		fmt.Println(fib(i))
+
+func newQueue(size int) *queue {
+	return &queue{
+		nodes: make([]*node, size),
+		size:  size,
 	}
 }
 
-/*func fibN(n int) *big.Int {
-	if n < 0 {
-		panic("fib n cant be negative")
+func (q *queue) push(n *node) {
+	if q.front == q.back && q.count > 0 {
+		nodes := make([]*node, len(q.nodes)+q.size)
+		copy(nodes, q.nodes[q.front:])
+		copy(nodes[len(q.nodes)-q.front:], q.nodes[:q.front])
+		q.front = 0
+		q.back = len(q.nodes)
+		q.nodes = nodes
 	}
-	a, _ := fib(n)
-	return a
-}*/
-
-func fib(n int) *big.Int { //new fib func
-	a := big.NewInt(1)
-	b := big.NewInt(1)
-	for i := 0; i < n; i++ {
-		a, b = b, big.NewInt(0).Add(a, b)
+	q.nodes[q.back] = n
+	q.back = (q.back + 1) % len(q.nodes)
+	q.count++
+}
+func (q *queue) pop() *node {
+	if q.count == 0 {
+		return nil
 	}
-	return a
+	node := q.nodes[q.front]
+	q.front = (q.front + 1) % len(q.nodes)
+	q.count--
+	return node
 }
 
-/*func fib(n int) (*big.Int, *big.Int) {
-	if n == 0 {
-		return big.NewInt(0), big.NewInt(1)
-	}
-	a, b := fib(n / 2)
-	c := big.NewInt(0).Mul(a, (big.NewInt(0).Sub(big.NewInt(0).Mul(b, big.NewInt(2)), a))) // a * ((b * 2) - a)
-	d := big.NewInt(0).Add(big.NewInt(0).Mul(a, a), big.NewInt(0).Mul(b, b))
-	if n%2 == 0 {
-		return c, d
-	} else {
-		return d, big.NewInt(0).Add(c, d)
-	}
-}*/
+type phones []int
+type node struct {
+	Value int
+}
 
+func (p phones) ViewList() {
+	for i, phone := range p {
+		fmt.Printf("\t %v) %v \n", i+1, phone)
+	}
+}
+func viewAddressBook(addressBook map[string]phones) {
+	for name, p := range addressBook {
+		fmt.Println(name)
+		p.ViewList()
+	}
+}
+func saveAddressBook(addressBook map[string]phones) {
+	file, _ := json.MarshalIndent(addressBook, "", "")
+
+	_ = ioutil.WriteFile("testoutput.json", file, 0644)
+}
+
+// PercentOf : [part] is X precent of [total]. return X
+func PercentOf(part int, total int) float64 {
+	return (float64(part) * float64(100)) / float64(total)
+}
+
+func vehInfo(obj vehilce) string {
+	x := PercentOf(obj.volumeOccup, obj.volume)
+	if x == 0 {
+		return "пустой"
+	}
+	if x < 20 {
+		return "Почти пустой"
+	}
+	if x >= 20 && x <= 40 {
+		return "Небольная заполненость"
+	}
+	if x > 40 && x < 60 {
+		return "Половина заполнена"
+	}
+	if x >= 60 && x < 100 {
+		return "Почти полный"
+	}
+	if x == 100 {
+		return "Полный"
+	}
+	return "obj.volumeOccup, obj.volume error"
+}
+func carInfo(obj car) string {
+	if obj.isRunning {
+		return "В пути"
+	}
+	return "Не в пути"
+
+}
+func printVehInfo(x vehilce) {
+	fmt.Println(x.model, vehInfo(x))
+}
+func printCarInfo(x car) {
+	fmt.Println(x.model, carInfo(x))
+}
 func main() {
-	var choice int
-	for choice < 1 || choice > 2 {
-		fmt.Printf("1 - fib\n2 - mass\n")
-		fmt.Scanln(&choice)
+	machina := vehilce{
+		model:        "gruzovik",
+		volume:       100,
+		volumeOccup:  82,
+		year:         2018,
+		isRunning:    true,
+		isWindowOpen: false,
 	}
-	switch choice {
-	case 1:
-		fibPrint(100)
-	case 2:
-		fillNumb()
-		fillPrime()
-		fmt.Print(prime, "\n")
+	machinka := vehilce{
+		model:        "gruzovichok",
+		volume:       50,
+		volumeOccup:  11,
+		year:         1999,
+		isRunning:    false,
+		isWindowOpen: true,
 	}
+	tachka := car{
+		model:        "cabrio",
+		volume:       15,
+		volumeOccup:  14,
+		year:         2020,
+		isRunning:    true,
+		isWindowOpen: true,
+	}
+	avtomobil := car{
+		model:        "lada",
+		volume:       10,
+		volumeOccup:  2,
+		year:         2007,
+		isRunning:    false,
+		isWindowOpen: true,
+	}
+
+	fmt.Println()
+
+	printVehInfo(machina)
+	printVehInfo(machinka)
+	printCarInfo(tachka)
+	printCarInfo(avtomobil)
+
+	fmt.Println()
+
+	queue := newQueue(1)
+	queue.push(&node{1})
+	queue.push(&node{22})
+	queue.push(&node{333})
+	fmt.Println(queue.pop(), queue.pop(), queue.pop())
+
+	fmt.Println()
+
+	addressBook := make(map[string]phones)
+
+	addressBook["Alex"] = phones{89996543210}
+	addressBook["Alex"] = append(addressBook["Alex"], 89993157365)
+	addressBook["Alex"] = append(addressBook["Alex"], 89999983121)
+	addressBook["Bob"] = phones{89167243812}
+	addressBook["Bob"] = append(addressBook["Bob"], 89161437244)
+	addressBook["Bob"] = append(addressBook["Bob"], 89163255424)
+	addressBook["Rob"] = phones{89264323453}
+	addressBook["Rob"] = append(addressBook["Rob"], 89260889352)
+	addressBook["Rob"] = append(addressBook["Rob"], 89265841488)
+
+	viewAddressBook(addressBook)
+
+	saveAddressBook(addressBook)
 }
