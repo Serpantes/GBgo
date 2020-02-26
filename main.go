4b74a129596c24597a116298f8f53404897a95d5
Serpantes <4uteppp@gmail.com>
2020-02-26 20:03:48 +0300

    lesson 4

--- main.go
+++ main.go
@@ -1,189 +1,183 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
+
+	calculator "./pkg"
 )
 
-type vehilce struct {
-	model                     string
-	volume, volumeOccup, year int
-	isRunning, isWindowOpen   bool
+//1
+type iStruct interface {
+	printStruct()
 }
-type car struct {
-	model        string
-	volume       int
-	volumeOccup  int
-	year         int
-	isRunning    bool
-	isWindowOpen bool
+type myStruct struct {
+	stuff     int
+	moreStuff int
 }
-
-type queue struct {
-	nodes []*node
-	size  int
-	front int
-	back  int
-	count int
+type anotherMyStruct struct {
+	stuff        int
+	moreStuff    int
+	anotherStuff int
 }
 
-func newQueue(size int) *queue {
-	return &queue{
-		nodes: make([]*node, size),
-		size:  size,
+func (s myStruct) printStruct() {
+	fmt.Println("stuff=", s.stuff)
+	fmt.Println("moreStuff=", s.moreStuff)
+}
+func (s anotherMyStruct) printStruct() {
+	fmt.Println("stuff=", s.stuff)
+	fmt.Println("moreStuff=", s.moreStuff)
+	fmt.Println("anotherStuff=", s.anotherStuff)
+}
+func doStuff(s iStruct) {
+	s.printStruct()
+}
+
+//2
+
+type person struct {
+	name  string
+	phone int
+}
+type phoneBook []person
+
+func (a phoneBook) Len() int           { return len(a) }
+func (a phoneBook) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a phoneBook) Less(i, j int) bool { return a[i].name < a[j].name }
+
+//4
+var coord = map[int]string{
+	1: "a",
+	2: "b",
+	3: "c",
+	4: "d",
+	5: "e",
+	6: "f",
+	7: "g",
+	8: "h",
+}
+
+type point struct {
+	x int
+	y int
+}
+
+func (p *point) getX() int {
+	return p.x
+}
+func (p *point) getY() int {
+	return p.y
+}
+
+func (p point) string() string {
+	return fmt.Sprintf("%s%d", coord[p.x], p.y)
+}
+func newPoint(x, y int) point {
+	return point{
+		x: x,
+		y: y,
 	}
 }
 
-func (q *queue) push(n *node) {
-	if q.front == q.back && q.count > 0 {
-		nodes := make([]*node, len(q.nodes)+q.size)
-		copy(nodes, q.nodes[q.front:])
-		copy(nodes[len(q.nodes)-q.front:], q.nodes[:q.front])
-		q.front = 0
-		q.back = len(q.nodes)
-		q.nodes = nodes
-	}
-	q.nodes[q.back] = n
-	q.back = (q.back + 1) % len(q.nodes)
-	q.count++
-}
-func (q *queue) pop() *node {
-	if q.count == 0 {
-		return nil
-	}
-	node := q.nodes[q.front]
-	q.front = (q.front + 1) % len(q.nodes)
-	q.count--
-	return node
+var availableMoves = []point{
+	newPoint(1, 2),
+	newPoint(-1, 2),
+	newPoint(2, 1),
+	newPoint(-2, 1),
+	newPoint(1, -2),
+	newPoint(-1, -2),
+	newPoint(-2, -1),
+	newPoint(2, -1),
 }
 
-type phones []int
-type node struct {
-	Value int
+type fig struct {
+	position point
 }
 
-func (p phones) ViewList() {
-	for i, phone := range p {
-		fmt.Printf("\t %v) %v \n", i+1, phone)
-	}
-}
-func viewAddressBook(addressBook map[string]phones) {
-	for name, p := range addressBook {
-		fmt.Println(name)
-		p.ViewList()
-	}
-}
-func saveAddressBook(addressBook map[string]phones) {
-	file, _ := json.MarshalIndent(addressBook, "", "")
-
-	_ = ioutil.WriteFile("testoutput.json", file, 0644)
+func (k *fig) getPos() *point {
+	return &k.position
 }
 
-// PercentOf : [part] is X precent of [total]. return X
-func PercentOf(part int, total int) float64 {
-	return (float64(part) * float64(100)) / float64(total)
+func (k *fig) calcAvail() []point {
+	res := make([]point, 0, 4)
+	for _, v := range availableMoves {
+		newX := k.getPos().getX() + v.getX()
+		newY := k.getPos().getY() + v.getY()
+		if isAvail(newX, newY) {
+			res = append(res, newPoint(newX, newY))
+		}
+	}
+
+	return res
 }
 
-func vehInfo(obj vehilce) string {
-	x := PercentOf(obj.volumeOccup, obj.volume)
-	if x == 0 {
-		return "пустой"
-	}
-	if x < 20 {
-		return "Почти пустой"
-	}
-	if x >= 20 && x <= 40 {
-		return "Небольная заполненость"
-	}
-	if x > 40 && x < 60 {
-		return "Половина заполнена"
-	}
-	if x >= 60 && x < 100 {
-		return "Почти полный"
-	}
-	if x == 100 {
-		return "Полный"
-	}
-	return "obj.volumeOccup, obj.volume error"
+func isAvail(x, y int) bool {
+	return x >= 1 && x <= 8 && y >= 1 && y <= 8
 }
-func carInfo(obj car) string {
-	if obj.isRunning {
-		return "В пути"
-	}
-	return "Не в пути"
 
+func newFig(x, y int) fig {
+	return fig{
+		position: point{
+			x: x,
+			y: y,
+		},
+	}
 }
-func printVehInfo(x vehilce) {
-	fmt.Println(x.model, vehInfo(x))
-}
-func printCarInfo(x car) {
-	fmt.Println(x.model, carInfo(x))
-}
+
 func main() {
-	machina := vehilce{
-		model:        "gruzovik",
-		volume:       100,
-		volumeOccup:  82,
-		year:         2018,
-		isRunning:    true,
-		isWindowOpen: false,
+	{ //1
+		mStr := &myStruct{
+			stuff:     4,
+			moreStuff: 42,
+		}
+		anotherMStr := &anotherMyStruct{
+			stuff:     4,
+			moreStuff: 42,
+		}
+		doStuff(mStr)
+		doStuff(anotherMStr)
 	}
-	machinka := vehilce{
-		model:        "gruzovichok",
-		volume:       50,
-		volumeOccup:  11,
-		year:         1999,
-		isRunning:    false,
-		isWindowOpen: true,
+	{ //2
+		var pBook phoneBook
+		pBook = append(pBook, person{
+			name:  "Vasya",
+			phone: 89999843177,
+		})
+		pBook = append(pBook, person{
+			name:  "Kolya",
+			phone: 89171234365,
+		})
+		pBook = append(pBook, person{
+			name:  "Nikita",
+			phone: 89255423145,
+		})
 	}
-	tachka := car{
-		model:        "cabrio",
-		volume:       15,
-		volumeOccup:  14,
-		year:         2020,
-		isRunning:    true,
-		isWindowOpen: true,
+	{ //3
+		calcHelp := "Spravka, ToDo" //2bDone
+		input := ""
+		for {
+			fmt.Print("> ")
+			if _, err := fmt.Scanln(&input); err != nil {
+				fmt.Println(err)
+				continue
+			}
+			if input == "help" {
+				fmt.Println(calcHelp)
+				continue
+			}
+			if input == "exit" {
+				break
+			}
+
+			if res, err := calculator.Calculate(input); err == nil {
+				fmt.Printf("Результат: %v\n", res)
+			} else {
+				fmt.Println("Не удалось произвести вычисление")
+			}
+		}
 	}
-	avtomobil := car{
-		model:        "lada",
-		volume:       10,
-		volumeOccup:  2,
-		year:         2007,
-		isRunning:    false,
-		isWindowOpen: true,
+	{ //4
+		f := newFig(2, 4)
+		fmt.Println(f.calcAvail())
 	}
-
-	fmt.Println()
-
-	printVehInfo(machina)
-	printVehInfo(machinka)
-	printCarInfo(tachka)
-	printCarInfo(avtomobil)
-
-	fmt.Println()
-
-	queue := newQueue(1)
-	queue.push(&node{1})
-	queue.push(&node{22})
-	queue.push(&node{333})
-	fmt.Println(queue.pop(), queue.pop(), queue.pop())
-
-	fmt.Println()
-
-	addressBook := make(map[string]phones)
-
-	addressBook["Alex"] = phones{89996543210}
-	addressBook["Alex"] = append(addressBook["Alex"], 89993157365)
-	addressBook["Alex"] = append(addressBook["Alex"], 89999983121)
-	addressBook["Bob"] = phones{89167243812}
-	addressBook["Bob"] = append(addressBook["Bob"], 89161437244)
-	addressBook["Bob"] = append(addressBook["Bob"], 89163255424)
-	addressBook["Rob"] = phones{89264323453}
-	addressBook["Rob"] = append(addressBook["Rob"], 89260889352)
-	addressBook["Rob"] = append(addressBook["Rob"], 89265841488)
-
-	viewAddressBook(addressBook)
-
-	saveAddressBook(addressBook)
 }
